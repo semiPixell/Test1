2ac4d7e2cbe3dfe485e01e283225d12c9cda3b50
semiPixell <akmaljavaid456@gmail.com>
2024-09-10 10:14:51 +1000

    cmit

---
+++ testapp1/testapp1/Business logic layer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testapp1
+{
+    internal class Business_logic_layer
+    {
+    }
+}
---
+++ testapp1/testapp1/classA.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testapp1
+{
+    public class classA
+    {
+       public string Name;
+
+        public classA()
+        {
+            Name = "";
+        }
+        public classA(string name)
+        {
+            Name = name;            
+        }
+    }
+}
